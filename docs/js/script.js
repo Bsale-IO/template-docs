e5de60250c0b2e377436efc7850fed9ace97cd34
iquinones-bsale <iquinones@imaginex.cl>
2021-05-20 20:09:07 -0400

    xx

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,13 @@
 (function(){
-    console.log("test")
+    var menu = querySelector("#page-menu")
+    if(menu){
+        var title = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
+        var items = "";
+        for(let i=0;i<title.length; i++){
+            items +=`<a class="menu-level-${title[i].localName}" href="#${title[i].id}">${title[i].innerText}</a>`;
+        }
+        menu.innerHTML = items
+    }
+
+
 })()
