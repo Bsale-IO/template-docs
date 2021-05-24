a6d7deca100787e7ea3b169514984ccf0725bb82
iquinones-bsale <iquinones@imaginex.cl>
2021-05-24 19:20:32 -0400

    +=

--- docs/assets/js/search.js
+++ docs/assets/js/search.js
@@ -75,7 +75,7 @@ function getQueryVariable(variable) {
   
         for (var i = 0; i < results.length; i++) {  // Iterate over the results
           var item = store[results[i].ref];
-          appendString =+ `<li>
+          appendString += `<li>
                           <a href="${item.url}">
                             <h3>${item.title}</h3>
                           </a>
