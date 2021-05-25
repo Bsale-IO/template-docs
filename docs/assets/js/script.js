f1260d39d0051d34b3a2e65d86fa529d962826e4
iquinones-bsale <iquinones@imaginex.cl>
2021-05-25 15:15:35 -0400

    layout page update

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -2,7 +2,7 @@
     var menu = document.querySelector("#page-menu")
     if(menu){
         var title = document.querySelectorAll("h2,h3,h4,h5,h6"); //no considera h1 por ser el titulo de la pagina
-        var items = "";
+        var items = "<strong>Menú Página</strong>";
         for(let i=0;i<title.length; i++){
             items +=`<a class="menu-level-${title[i].localName}" href="#${title[i].id}">${title[i].innerText}</a>`;
         }
